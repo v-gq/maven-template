76d4fa09955476ec02ab5f0f9da0bd62a5784d9f
v <v@192.168.0.101>
2020-08-09 14:27:04 +0800

    first commit

---
+++ maven-archetype-spring-boot-gq/src/test/java/com/gq/TestApplicatonTests.java
@@ -0,0 +1,20 @@
+package com.gq;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+* TestApplicatonTests
+*/
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestApplicatonTests {
+
+	@Test
+	public void contextLoads(){
+		
+	}
+	
+}
---
+++ maven-archetype-spring-boot-gq/target/generated-sources/archetype/src/main/resources/archetype-resources/src/test/java/TestApplicatonTests.java
@@ -0,0 +1,23 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+* TestApplicatonTests
+*/
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestApplicatonTests {
+
+	@Test
+	public void contextLoads(){
+		
+	}
+	
+}
